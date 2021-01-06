3af6b431a5262b061edc0ea7e6d8eca95e4e4fb7
cdrann <a.redko@g.nsu.ru>
2021-01-06 17:52:32 +0700

    Commit text

---
+++ Transaction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMproject;
+
+namespace ATM
+{
+    public abstract class Transaction
+    {
+        private ATMproject.ATM currATM;
+        private Account currAccount;
+
+        public Transaction(ATMproject.ATM _currATM, Account _currAccount)
+        {
+            currATM = _currATM;
+            currAccount = _currAccount;
+        }
+
+        public ATMproject.ATM CurrATM
+        {
+            get
+            {
+                return currATM;
+            }
+        }
+
+        public ATMproject.Account CurrAccount
+        {
+            get
+            {
+                return currAccount;
+            }
+        }
+
+        public abstract void Execute();
+    }
+}
